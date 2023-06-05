b708c14eefb333bb2df12d27ace8f351d97f27a6
Abhinandan Aryal <anudwigna@gmail.com>
2023-06-05 18:22:02 +0200

    removed unittest import

--- day_two.py
+++ day_two.py
@@ -3,9 +3,6 @@
 # #### For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. 
 # If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
-import unittest as ut
-
-
 def day_two(input: list):
     prod = 1
     output = []
